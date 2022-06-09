92881f013c2c25f28875b10ff05e648170b95c64
MARIN <josermarinr18@gmail.com>
2022-06-09 17:37:27 +0200

    testing hooks

--- src/packages/LazyLoad/LazyLoad.tsx
+++
@@ -1,9 +0,0 @@
-import { FunctionalComponent } from "vue"
-import data from './data.json'
-
- export const LazyLoad: FunctionalComponent<{ numberOfImage: Number}> = ({ numberOfImage }, slots) => {
- 
-    return  <>{data.wolves.map(wolf => <img v-lazy={{src:wolf.images[0]}}/>)}</>
-
- }
- 
