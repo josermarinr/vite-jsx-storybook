4d70666ad0bdb2aab983c32deb8817832872a7c0
MARIN <josermarinr18@gmail.com>
2022-06-15 14:16:33 +0200

    Fix: relative path

--- .storybook/main.js
+++ .storybook/main.js
@@ -41,6 +41,7 @@ module.exports = {
     // return the customized config
     return mergeConfig(config, {
       // customize the Vite config here
+      base: './',
       plugins: [vue({
         customElement: true
       }), vueJsx()],
